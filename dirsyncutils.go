c388280cc40ce74d39c8f89739aa08dcb19cdc66
Simon Tucker <brotherlogic@gmail.com>
2020-03-10 18:08:58 -0700

    Adds ability to hydrate server list.

--- dirsyncutils.go
+++ dirsyncutils.go
@@ -1,5 +1,37 @@
 package main
 
-func doNothing() {
-	// pass
+import (
+	"os"
+
+	"golang.org/x/net/context"
+)
+
+func (s *Server) hydrate(ctx context.Context) error {
+	config, err := s.load(ctx)
+	if err != nil {
+		return err
+	}
+
+	change := false
+	for _, sync := range config.GetSyncs() {
+		_, err := os.Stat(sync.GetDir())
+		if err == nil {
+			found := false
+			for _, server := range sync.GetServers() {
+				if server == s.Registry.Name {
+					found = true
+				}
+			}
+
+			if !found {
+				sync.Servers = append(sync.Servers, s.Registry.Name)
+				change = true
+			}
+		}
+	}
+
+	if change {
+		return s.save(ctx, config)
+	}
+	return nil
 }
--- dirsyncutils_test.go
+++ dirsyncutils_test.go
@@ -2,8 +2,37 @@ package main
 
 import (
 	"testing"
+
+	pb "github.com/brotherlogic/dirsync/proto"
+	"golang.org/x/net/context"
 )
 
-func TestNothing(t *testing.T) {
-	doNothing()
+func TestHydrate(t *testing.T) {
+	s := InitTestServer()
+	s.AddSync(context.Background(), &pb.AddSyncRequest{Sync: &pb.Sync{Dir: "/etc/"}})
+
+	err := s.hydrate(context.Background())
+	if err != nil {
+		t.Errorf("Bad hydrate: %v", err)
+	}
+
+	err = s.hydrate(context.Background())
+	if err != nil {
+		t.Errorf("Bad hydrate: %v", err)
+	}
+
+	config, _ := s.load(context.Background())
+
+	if len(config.GetSyncs()[0].Servers) != 1 {
+		t.Errorf("Sync was not hydrated")
+	}
+}
+
+func TestLoadFailHydrate(t *testing.T) {
+	s := InitTestServer()
+	s.GoServer.KSclient.Fail = true
+	err := s.hydrate(context.Background())
+	if err == nil {
+		t.Errorf("No error on bad hydrate")
+	}
 }
