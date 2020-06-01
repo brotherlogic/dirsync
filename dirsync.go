59d0ed8832d238767e62648b22da27e86e9e9383
Simon Tucker <brotherlogic@gmail.com>
2020-06-01 14:58:02 -0700

    Bounces dirsync

--- dirsync.go
+++ dirsync.go
@@ -77,9 +77,7 @@ func (s *Server) Mote(ctx context.Context, master bool) error {
 
 // GetState gets the state of the server
 func (s *Server) GetState() []*pbg.State {
-	return []*pbg.State{
-		&pbg.State{Key: "syncs", Text: fmt.Sprintf("%v", s.config.GetSyncs())},
-	}
+	return []*pbg.State{}
 }
 
 func (s *Server) runTimedSync(ctx context.Context) (time.Time, error) {
